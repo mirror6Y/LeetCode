0ee9cf1fabf3f76cb6cd435e091e18c6b79a81e7
mirror6Y <1766650501@qq.com>
2020-05-11 18:59:24 +0800

    27移除元素

---
+++ src/main/java/number27/Solution.java
@@ -0,0 +1,33 @@
+package number27;
+
+/**
+ * @author mirror6
+ */
+public class Solution {
+
+    public int removeElement(int[] nums, int val) {
+
+        int i = 0;
+        int j = nums.length - 1;
+
+        //注意：这里的j不是数组长度
+        while (i <= j) {
+            if (nums[i] == val) {
+                nums[i] = nums[j];
+                j--;
+            } else {
+                i++;
+            }
+        }
+
+        for (; i < j; ) {
+            if (nums[i] == val) {
+                nums[i] = nums[j];
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return j + 1;
+    }
+}
