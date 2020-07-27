0a25f03c2fa0cf04a173480a4fe50562dd434372
mirror6Y <1766650501@qq.com>
2020-07-27 22:10:55 +0800

    19 添加辅助接点 处理特殊情况

--- src/main/java/twopoint/Code19.java
+++ src/main/java/twopoint/Code19.java
@@ -18,10 +18,14 @@ public class Code19 {
     }
 
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode r = head;
+        ListNode p = r;
+        ListNode q = r;
 
-        ListNode p = head;
-        ListNode q = head;
-        while (n > 0) {
+        while (n >= 0 && q != null) {
             q = q.next;
             n--;
         }
@@ -31,6 +35,32 @@ public class Code19 {
             q = q.next;
         }
         p.next = p.next.next;
-        return p;
+        return r;
+    }
+
+    public ListNode removeNthFromEnd1(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        // Advances first pointer so that the gap between first and second is n nodes apart
+        for (int i = 1; i <= n + 1; i++) {
+            first = first.next;
+        }
+        // Move first to the end, maintaining the gap
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+
+
+    public static void main(String[] args) {
+        Code19 code19 = new Code19();
+        ListNode node = new ListNode(1);
+        node.next = new ListNode(2);
+        code19.removeNthFromEnd(node, 2);
     }
 }
